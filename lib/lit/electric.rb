0f2acc770d79056d763aed72b3a8d6342ebeef41
Emi Ko <emi8emi@gmail.com>
2019-05-29 10:43:39 -0700

    first commit

---
+++ lib/lit/electric.rb
@@ -0,0 +1,7 @@
+require "lit/electric/version"
+
+module Lit
+  module Electric
+    # Your code goes here...
+  end
+end
---
+++ lib/lit/electric/version.rb
@@ -0,0 +1,5 @@
+module Lit
+  module Electric
+    VERSION = "0.1.0"
+  end
+end
